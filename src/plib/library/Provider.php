12839def8167b2027e73d6686a1f6f3737170283
Sergey Oleynikov <oley@Sergeys-MacBook-Pro.local>
2017-03-25 23:09:37 +0100

    updated API for Provider

--- src/plib/library/Provider.php
+++ src/plib/library/Provider.php
@@ -6,8 +6,12 @@ use Modules_PleskCloudProviders_Provider\VpsProviderInterface;
 
 class Modules_PleskCloudProviderExample_Provider implements VpsProviderInterface
 {
+    /**
+    * Start deployplment proces. 
+    */
     public function deployDump()
     {
+        /*
         $ip = '10.52.49.78';
         $hostname = 'w10-52-49-78.qa.plesk.ru';
         $password = 'setup';
@@ -16,7 +20,52 @@ class Modules_PleskCloudProviderExample_Provider implements VpsProviderInterface
         $dump->ipv4 = [$ip];
         $dump->ipv6 = [];
         $dump->password = $password;
+        */
+        return $dump_id;
+    }
+
+    public function isDumpReady($dump_id)
+    {
+    }
+
+    /**
+     * The method will be called after isDumpRead returns true. 
+     * There is make sense to implement this method if dump is needed for some extra preparation like generating admin password
+     * or push admin info. 
+     * opt = array (
+            'admin_info' = array (
+                'company' =>,
+                'name' => ,
+                'phone' =>, 
+                'fax'=>, 
+                'email' =>,
+                'address' =>,
+                'city' => 
+                'state' => 
+                'zip' => 
+                'country'=> , 
+            )
+        )
+     * 
+     */
+    public function prepareDump($dump_id, $opt)
+    {
+    }
+
+    public function getDumpInfo($dump_id)
+    {
+         /*
+        $ip = '10.52.49.78';
+        $hostname = 'w10-52-49-78.qa.plesk.ru';
+        $password = 'setup';
+
+        $dump = new Dump();
+        $dump->ipv4 = [$ip];
+        $dump->ipv6 = [];
+        $dump->password = $password;
+
         return $dump;
+        */
     }
 
     public function destroyDump(Dump $dump)
